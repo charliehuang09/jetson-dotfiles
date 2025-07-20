2324aa79e20cfb9396e30537185b90c490b6a281
Charlie Huang <charliehuang09@gmail.com>
2025-07-19 20:57:55 -0700

    reorder install.sh
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- install.sh
+++ install.sh
@@ -17,16 +17,17 @@ curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh 
 curl -L https://raw.githubusercontent.com/catppuccin/gnome-terminal/v1.0.0/install.py | python3 -
 sudo fc-cache -fv
 
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-rm /home/nvidia/.zshrc
-
 git submodule init 
 git submodule update
 
 sudo apt install clangd-16
 ln -s /usr/bin/clangd-16 ~/.local/share/nvim/mason/bin/clangd
 mkdir ~/.local/share/nvim/mason/packages/clangd
+
+stow .
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm /home/nvidia/.zshrc
 
 stow .
